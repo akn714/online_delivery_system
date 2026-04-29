6acf5126a430cc20bef06e0bb8a747b1a394c69f
akn714 <adarshkumar20012704@gmail.com>
2026-04-30 02:54:39 +0530

    fixed main.py for render

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,9 @@ async def root():
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run("main:app", reload=True)
-    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    
+
+    port = int(os.environ.get("PORT", 4000))
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
